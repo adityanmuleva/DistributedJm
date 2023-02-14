bf6bd10422165d31dfe3b796aa0676c5e4d8dbc4
adityanmuleva <aadityamuleva@gmail.com>
2023-02-14 13:39:49 +0530

    updating docker images and kubernetes ports

--- DockerJmeter/test.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-command = "jmeter -n -t jmx/light_thread.jmx -l ./run_results/test_result.jtl"
-result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
-if result:
-    print("Process Completed")
